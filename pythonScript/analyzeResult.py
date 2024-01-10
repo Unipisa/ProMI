1f71c222bc4631e6a773a007c5c78ba2b7cacc59
Pham Tran Huong Giang <giangpth@host-131-114-215-179.m.unipi.it>
2024-01-10 16:06:41 +0100

    Update the website

--- pythonScript/analyzeResult.py
+++ pythonScript/analyzeResult.py
@@ -75,7 +75,7 @@ def calRelVar(res, thres, debug=False): # calculate relative variance and compar
             
         res[key]['mean'] = mean
         res[key]['relVar'] = var
-        # print(key, res[key]['mean'])
+        print(key, res[key]['mean'])
     return res 
 
 def getWtMut(res, debug=False):
@@ -133,7 +133,7 @@ def main():
     # for i in [0.05]:
     for i in (1000.0, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.09, 0.08, 0.07, 0.06, 0.05, 0.04, 0.03, 0.02, 0.01, 0):
         print("With threshold {}:".format(i))
-        # work with alphafold result
+        # work with alphafold result 
         print("Alphafold result")
         alres = calRelVar(alres, i, debug)
         alcorr = checkRes(alres, debug)
